4db51c637eeb255cef1a74a73bbc4bea72cd7e18
Lil2415 <136098662+Lil2415@users.noreply.github.com>
2023-07-14 11:35:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,15 +14,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['15517868003'] //ur owner number
+global.owner = ['254706697688'] //ur owner number
 global.ownername = "WHATSAPP OFC" //ur owner name
 global.ytname = "SANDARU-YT" //ur yt chanel name
 global.socialm = "GitHub: sandaru07" //ur github or insta name
 global.location = "asia, colombo, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "15517868003" //ur number
-global.premium = ['15517868003'] //ur premium number
+global.ownernomer = "254706697688" //ur number
+global.premium = ['254706697688'] //ur premium number
 global.botname = 'Xcoco Md' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
